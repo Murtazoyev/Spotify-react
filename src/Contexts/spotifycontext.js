8ace30cb26cb426d0d7a6bb77df3deeb9833861c
DilshodM <dilshodmurtazoyev5@gmail.com>
2023-08-27 13:55:00 +0500

    redirect url

--- src/Contexts/spotifycontext.js
+++ src/Contexts/spotifycontext.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 const spotify = createContext({
     client_id: "44f2d95737104a19943990a814afccbd",
-    REDIRECT_URI: "https://weprospoty.netlify.app/",
+    REDIRECT_URI: "https://main--delightful-daifuku-f7f604.netlify.app/",
     // REDIRECT_URI: "http://localhost:3000",
     AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
     RESPONSE_TYPE: "token",
